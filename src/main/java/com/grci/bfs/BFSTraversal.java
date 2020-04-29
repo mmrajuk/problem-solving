b0acb6507d1d845f4771a5f13ad9d0161d0990f9
Murali <muraju@adobe.com>
2020-04-28 17:44:11 -0700

    BFS easy problems & solutions

---
+++ src/main/java/com/grci/bfs/BFSTraversal.java
@@ -0,0 +1,189 @@
+package com.grci.bfs;
+
+import com.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+public class BFSTraversal {
+
+
+    /**
+     *
+     * @param root
+     *
+     * Given a binary tree, populate an array to represent its level-by-level traversal.
+     * You should populate the values of all nodes of each level from left to right in separate sub-arrays.
+     *
+     * Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, i.e., the lowest level comes first.
+     * You should populate the values of all nodes in each level from left to right in separate sub-arrays.
+     */
+    public List<List<Integer>> levelOrderTraversal(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null)
+            return null;
+        Queue<TreeNode> queue = new LinkedList<>();//Queue is implemented using Linked List in Java
+        queue.add(root);
+        while(!queue.isEmpty()){
+            //level size to iterate over
+            int lSize = queue.size();
+            //placeholder for holding elements in a level
+            List<Integer> lElements = new ArrayList<>();
+            for(int i=0; i<lSize; i++) {
+                TreeNode node = queue.poll();
+                lElements.add(node.val);
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+            result.add(lElements);
+        }
+        return result;
+    }
+
+
+    /**
+     *
+     * @param root
+     * @return
+     *
+     * Given a binary tree, populate an array to represent its level-by-level traversal in reverse order,
+     * i.e., the lowest level comes first.
+     * You should populate the values of all nodes in each level from left to right in separate sub-arrays.
+     */
+    public List<List<Integer>> reverseLevelOrderTraversal(TreeNode root){
+        List<List<Integer>> result = new LinkedList<>();
+        if(root == null)
+            return null;
+        Queue<TreeNode> queue = new LinkedList<>();//Queue is implemented using Linked List in Java
+        queue.add(root);
+        while(!queue.isEmpty()){
+            //level size to iterate over
+            int lSize = queue.size();
+            //placeholder for holding elements in a level
+            List<Integer> lElements = new ArrayList<>();
+            for(int i=0; i<lSize; i++) {
+                TreeNode node = queue.poll();
+                lElements.add(node.val);
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+            result.add(0,lElements);
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param root
+     * @return
+     *
+     * Given a binary tree, populate an array to represent its zigzag level order traversal.
+     * You should populate the values of all nodes of the first level from left to right,
+     * then right to left for the next level and keep alternating in the same manner for the following levels.
+     */
+    public List<List<Integer>> zigZagTraversal(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null)
+            return null;
+        Queue<TreeNode> queue = new LinkedList<>();//Queue is implemented using Linked List in Java
+        queue.add(root);
+        boolean zigZag = false; //every alternate level should be
+        while(!queue.isEmpty()){
+            //level size to iterate over
+            int lSize = queue.size();
+            //placeholder for holding elements in a level
+            List<Integer> lElements = new ArrayList<>();
+            for(int i=0; i<lSize; i++) {
+                TreeNode node = queue.poll();
+                if(zigZag)
+                    lElements.add(0,node.val);
+                else
+                    lElements.add(node.val);
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+
+            result.add(lElements);
+            zigZag = !zigZag;
+        }
+        return result;
+    }
+
+
+    /**
+     *
+     * @param root
+     * @return
+     *
+     * Given a binary tree, populate an array to represent the averages of all of its levels.
+     */
+    public List<Double> calculateLevelAverage(TreeNode root){
+
+        List<Double> averages = new ArrayList<>();
+        if(root == null)
+            return null;
+        Queue<TreeNode> queue = new LinkedList<>();//Queue is implemented using Linked List in Java
+        queue.add(root);
+        while(!queue.isEmpty()){
+            //level size to iterate over
+            int lSize = queue.size();
+            //placeholder for holding elements in a level
+            List<Integer> lElements = new ArrayList<>();
+            for(int i=0; i<lSize; i++) {
+                TreeNode node = queue.poll();
+                lElements.add(node.val);
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+            averages.add(lElements.stream().mapToInt(ele -> ele).average().getAsDouble());
+        }
+        return averages;
+
+    }
+
+
+    /**
+     *
+     * @param root
+     * @return
+     *
+     * Find the minimum depth of a binary tree.
+     * The minimum depth is the number of nodes along the shortest path from the root node to the nearest leaf node.
+     */
+    public int findMinDepth(TreeNode root){
+        if(root == null)
+            return 0;
+        Queue<TreeNode> queue = new LinkedList<>();//Queue is implemented using Linked List in Java
+        queue.add(root);
+        int mindDepth = 1;//root node.
+        while(!queue.isEmpty()){
+            //level size to iterate over
+            int lSize = queue.size();
+            for(int i=0; i<lSize; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+                if(node.left==null && node.right==null)
+                    return mindDepth;
+            }
+            mindDepth++;
+        }
+        return mindDepth;
+
+    }
+
+}
--- src/test/java/com/grci/bfs/BFSTraversalTest.java
+++ src/test/java/com/grci/bfs/BFSTraversalTest.java
@@ -4,11 +4,12 @@ import com.common.TreeNode;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-public class BFSTests {
+public class BFSTraversalTest {
 
     @Test
     public void testLevelOrderTraversal(){
@@ -19,8 +20,8 @@ public class BFSTests {
         root.right.left = new TreeNode(10);
         root.right.right = new TreeNode(5);
 
-        BFSProblems bfs = new BFSProblems();
-        List<List<Integer>> result = bfs.levelOrderTraversal(root, false);
+        BFSTraversal bfs = new BFSTraversal();
+        List<List<Integer>> result = bfs.levelOrderTraversal(root);
         assertEquals(Lists.newArrayList(Lists.newArrayList(12),Lists.newArrayList(7,1),
                 Lists.newArrayList(9,10,5)),result);
     }
@@ -34,8 +35,8 @@ public class BFSTests {
         root.right.left = new TreeNode(10);
         root.right.right = new TreeNode(5);
 
-        BFSProblems bfs = new BFSProblems();
-        List<List<Integer>> result = bfs.levelOrderTraversal(root, true);
+        BFSTraversal bfs = new BFSTraversal();
+        List<List<Integer>> result = bfs.reverseLevelOrderTraversal(root);
         assertEquals(Lists.newArrayList(Lists.newArrayList(9,10,5),Lists.newArrayList(7,1),
                 Lists.newArrayList(12)),result);
     }
@@ -51,9 +52,39 @@ public class BFSTests {
         root.right.left.left = new TreeNode(20);
         root.right.left.right = new TreeNode(17);
 
-        BFSProblems bfs = new BFSProblems();
+        BFSTraversal bfs = new BFSTraversal();
         List<List<Integer>> result = bfs.zigZagTraversal(root);
         assertEquals(Lists.newArrayList(Lists.newArrayList(12),Lists.newArrayList(1,7),
                 Lists.newArrayList(9,10,5),Lists.newArrayList(17,20)),result);
+    }
+
+    @Test
+    public void testLevelAverage(){
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+
+        BFSTraversal bfs = new BFSTraversal();
+        List<Double> result = bfs.calculateLevelAverage(root);
+        assertEquals(Lists.newArrayList(12.0,4.0,6.5),result);
+    }
+
+    @Test
+    public void testMinDepth(){
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+
+        BFSTraversal bfs = new BFSTraversal();
+        assertEquals(2,bfs.findMinDepth(root));
+        root.left.left = new TreeNode(9);
+        root.right.left.left = new TreeNode(11);
+        assertEquals(3,bfs.findMinDepth(root));
     }
 }
